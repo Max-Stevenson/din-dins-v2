e1dea78a909e366423865052a30aca279be7b79f
Max Stevenson <maxstevenson@msn.com>
2022-01-15 12:06:11 -0600

    - First draft of recipe schema.

--- backend/src/models/recipe.js
+++ backend/src/models/recipe.js
@@ -3,7 +3,16 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   name: String,
-  
+  servings: Number,
+  cookingTime: Number,
+  isVegetarian: Boolean,
+  ingredients: [{
+    quantity: Number,
+    measure: String,
+    ingredient: String
+  }],
+  method: [String],
+  tags: [String],
 });
 
 module.exports = mongoose.model("Recipe", recipeSchema);
