a39a232ca77633ff598ea665664b65c64988bca4
Max Stevenson <maxstevenson@msn.com>
2022-01-17 20:56:50 -0600

    - Added further tests for recipe.

--- backend/tests/recipe.spec.js
+++ backend/tests/recipe.spec.js
@@ -5,3 +5,10 @@ const { sampleRcipe, sampleRcipeTwo } = require("./utils/testData");
 it("returns 201 status when create recipe request is valid", (done) => {
   request(app).post("/api/v1/recipes").send(sampleRcipe).expect(201, done);
 });
+
+it("returns recipe object when create recipe request is done", (done) => {
+  request(app).post("/api/v1/recipes").send(sampleRcipe).then((res) => {
+    expect(res.body).toContainEqual(sampleRcipe);
+    done();
+  });
+});
