cd3c532bf828b7d5d596d17021a24c708bcf0fce
Max Stevenson <maxstevenson@msn.com>
2022-01-18 20:54:17 -0600

    - Modified recipe scheme to include validation.

--- backend/src/models/recipe.js
+++ backend/src/models/recipe.js
@@ -1,18 +1,25 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+
+const { Schema } = mongoose;
 
 const recipeSchema = new Schema({
-  name: String,
-  servings: Number,
-  cookingTime: Number,
-  isVegetarian: Boolean,
-  ingredients: [{
-    quantity: Number,
-    measure: String,
-    ingredient: String
-  }],
-  method: [String],
-  tags: [String],
+  name: { type: String, required: true },
+  servings: { type: Number, required: true },
+  cookingTime: { type: Number, required: true },
+  isVegetarian: { type: Boolean, required: true },
+  ingredients: {
+    _id: false,
+    type: [
+      {
+        quantity: { type: Number, required: true },
+        measure: { type: String, required: true },
+        ingredient: { type: String, required: true },
+      },
+    ],
+    required: true,
+  },
+  method: { type: [String], required: true },
+  tags: { type: [String] },
 });
 
 module.exports = mongoose.model("Recipe", recipeSchema);
