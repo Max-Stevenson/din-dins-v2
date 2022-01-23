00b7a54cd6eb1ff2933d2182173c3bbb1fc1c28b
Max Stevenson <maxstevenson@msn.com>
2022-01-22 20:53:02 -0600

    - Modified existing recipe routes. - Added delete route for recipe.

--- backend/src/controllers/recipesController.js
+++ backend/src/controllers/recipesController.js
@@ -1,16 +1,17 @@
 // Contains middleware logic for recipes routes.
 const HttpError = require("../models/httpError");
 const Recipe = require("../models/recipe");
+const ValidationError = require("../models/validationError");
 
-const getAllRecipes = async (req, res, next) => {
+const getAllRecipes = async (_req, res, next) => {
   let recipes;
 
   try {
     recipes = await Recipe.find();
-    res.status(200).send({ recipes });
   } catch (err) {
     next(new HttpError("Could not get recipes, please try again.", 500));
   }
+  res.status(200).send({ recipes });
 };
 
 const getRecipeById = async (req, res, next) => {
@@ -22,29 +23,46 @@ const getRecipeById = async (req, res, next) => {
     if (!recipe) {
       next(new HttpError("Recipe does not exist, please try again.", 404));
     }
-    res.status(200).send({ recipe });
   } catch (err) {
     next(new HttpError("Could not get recipe, please try again.", 500));
   }
+  res.status(200).send({ recipe });
 };
 
 const createRecipe = async (req, res, next) => {
+  const recipe = new Recipe(req.body);
+
   try {
-    const recipe = await new Recipe(req.body).save();
-    res.status(201).send(recipe);
+    await recipe.save();
   } catch (err) {
     const errorDetails = {};
     Object.keys(err.errors).forEach((key) => {
       errorDetails[key] = err.errors[key].message;
     });
-    const error = new HttpError("Error creating recipe", errorDetails, 400);
+    const error = new ValidationError("Invalid input", errorDetails, 400);
     next(error);
   }
+  res.status(201).send(recipe);
 };
 
 const updateRecipe = (req, res, next) => {};
 
-const deleteRecipe = (req, res, next) => {};
+const deleteRecipe = async (req, res, next) => {
+  let recipe;
+  const { id } = req.params;
+  try {
+    recipe = await Recipe.findByIdAndDelete(id);
+    if (!recipe) {
+      next(
+        new HttpError("Could not find recipe to delete, please try again.", 404)
+      );
+    }
+  } catch (err) {
+    next(new HttpError("Could not delete recipe, please try again.", 500));
+  }
+
+  res.status(200).send(recipe);
+};
 
 exports.getAllRecipes = getAllRecipes;
 exports.getRecipeById = getRecipeById;
