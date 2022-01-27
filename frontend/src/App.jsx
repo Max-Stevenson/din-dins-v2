fbf00c9266c0648eb836f176c53e90425ab1ba71
Max Stevenson <maxstevenson@msn.com>
2022-01-26 20:17:42 -0600

    - eslint setup on frontend.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          {" "}
+          <code>src/App.js</code>
+          {" "}
+          and save to reload.
         </p>
         <a
           className="App-link"
