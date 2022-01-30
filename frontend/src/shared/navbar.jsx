85d5df8a6a140190f2e1ec7274691c96f15bd331
Max Stevenson <maxstevenson@msn.com>
2022-01-30 13:37:12 -0600

    - Added more navbar styles.

--- frontend/src/shared/navbar.jsx
+++ frontend/src/shared/navbar.jsx
@@ -5,9 +5,9 @@ import "./Navbar.scss";
 function Navbar() {
   return (
     <div className="navbar-container">
-      <NavButton icon="cog" />
       <NavButton icon="utensils" />
       <NavButton icon="calendar-alt" />
+      <NavButton icon="cog" />
     </div>
   );
 }
