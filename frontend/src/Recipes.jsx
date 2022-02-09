ff791d3de6accd65d43235c5a1a23538fb54e9d0
Max Stevenson <maxstevenson@msn.com>
2022-02-09 16:59:48 -0600

    - Added image placeholder.

--- frontend/src/Recipes.jsx
+++ frontend/src/Recipes.jsx
@@ -3,6 +3,9 @@ import React from "react";
 import { Grid } from "@mui/material";
 import { Link } from "react-router-dom";
 import useFetch from "./useFetchHook";
+import "./Recipes.scss";
+
+const logo = require("./recipe.jpg");
 
 function Recipes() {
   const { data, error, loading } = useFetch(
@@ -15,15 +18,20 @@ function Recipes() {
   if (!loading && data) {
     // eslint-disable-next-line array-callback-return
     const recipeItems = data.map((recipe) => (
-      <Grid item xs={6} sm={6} md={3}>
+      <Grid key={recipe._id} item xs={6} sm={6} md={3}>
         <Link to={`/recipes/view/${recipe._id}`}>
-          <div key={recipe._id}>
+          <div>
+            <img alt="" src={logo} />
             <h3>{recipe.name}</h3>
           </div>
         </Link>
       </Grid>
     ));
-    return <Grid container>{recipeItems}</Grid>;
+    return (
+      <div className="wrapper">
+        <Grid container>{recipeItems}</Grid>
+      </div>
+    );
   }
 
   return <h2>Loading</h2>;
