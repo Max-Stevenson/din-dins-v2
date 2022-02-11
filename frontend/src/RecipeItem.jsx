f3748098f31b393ed8285668768c2c6cbc5b257e
Max Stevenson <maxstevenson@msn.com>
2022-02-11 17:41:39 -0600

    - Fleshing out RecipeItem page.

--- frontend/src/RecipeItem.jsx
+++ frontend/src/RecipeItem.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function RecipeItem() {
+function RecipeItem({ recipeItem }) {
   return (
     <div className="recipe-item__wrapper" />
   );
 }
+
+RecipeItem.propTypes = {
+  recipeItem: PropTypes.shape({
+    _id: PropTypes.string
+  }).isRequired
+};
 
 export default RecipeItem;
