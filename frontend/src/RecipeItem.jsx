b5c41e50b7dfe76aa2b449a22bd5ee11f3736d52
Max Stevenson <maxstevenson@msn.com>
2022-02-12 18:08:06 -0600

    - Finished propType for recipeItem page.

--- frontend/src/RecipeItem.jsx
+++ frontend/src/RecipeItem.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import PropTypes from "prop-types";
+import PropTypes, { string } from "prop-types";
 
 function RecipeItem({ recipeItem }) {
   return (
@@ -9,7 +9,19 @@ function RecipeItem({ recipeItem }) {
 
 RecipeItem.propTypes = {
   recipeItem: PropTypes.shape({
-    _id: PropTypes.string
+    _id: PropTypes.string,
+    name: PropTypes.string,
+    servings: PropTypes.number,
+    cookingTime: PropTypes.number,
+    isVegetarian: PropTypes.bool,
+    ingredients: PropTypes.arrayOf(PropTypes.shape({
+      quantity: PropTypes.number,
+      measure: PropTypes.string,
+      ingredient: PropTypes.string
+    })),
+    method: PropTypes.arrayOf(string),
+    tags: PropTypes.arrayOf(string),
+    __v: PropTypes.string
   }).isRequired
 };
 
