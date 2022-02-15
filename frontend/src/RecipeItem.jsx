a9d452296a5001dbab677a5a75879fdc0314bd6c
Max Stevenson <maxstevenson@msn.com>
2022-02-14 22:14:26 -0600

    - WIP implementing tabs in RecipeItem.

--- frontend/src/RecipeItem.jsx
+++ frontend/src/RecipeItem.jsx
@@ -1,27 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Tab, Tabs } from "@mui/material";
-import { TabPanel } from "@material-ui/lab";
 
 const logo = require("./recipe.jpg");
 
 function RecipeItem({ recipeItem }) {
+  const onChange = () => {
+  };
+
   return (
     <div className="recipe-item__wrapper">
       <div className="recipe-item__img-wrapper">
         <img alt="" src={logo} />
       </div>
       <h3>{recipeItem.name}</h3>
-      <Tabs>
+      <Tabs onChange={() => onChange()} defaultSelectedIndex={1}>
         <Tab label="Ingredients" value="1" />
         <Tab label="Method" value="2" />
       </Tabs>
-      <TabPanel value="1">
-        <h2>Test</h2>
-      </TabPanel>
-      <TabPanel value="2">
-        <h2>Test Again</h2>
-      </TabPanel>
     </div>
   );
 }
@@ -42,7 +38,7 @@ RecipeItem.propTypes = {
     ),
     method: PropTypes.arrayOf(PropTypes.string),
     tags: PropTypes.arrayOf(PropTypes.string),
-    __v: PropTypes.string
+    __v: PropTypes.number
   }).isRequired
 };
 
