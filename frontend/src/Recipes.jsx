20345055b4b2fcf5b40b8da6f55b3df63db180af
Max Stevenson <maxstevenson@msn.com>
2022-02-16 16:18:34 -0600

    - Back to using individual routes for recipe view.

--- frontend/src/Recipes.jsx
+++ frontend/src/Recipes.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable no-underscore-dangle */
-import React, { useState } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 import { Grid } from "@mui/material";
 import LoadingSpinner from "./shared/LoadingSpinner";
 import useFetch from "./useFetchHook";
 import "./Recipes.scss";
-import RecipeItem from "./RecipeItem";
 import DisplayWrapper from "./shared/DisplayWrapper";
 
 const logo = require("./recipe.jpg");
@@ -14,20 +14,7 @@ function Recipes() {
     "http://localhost:3000/api/v1/recipes"
   );
 
-  const [isRecipeView, setIsRecipeView] = useState(false);
-  const [selectedRecipe, setSelectedRecipe] = useState();
-
-  const selectRecipe = (recipeId) => {
-    const recipe = data.find((r) => r._id === recipeId);
-    if (recipe) {
-      setSelectedRecipe(recipe);
-      setIsRecipeView(true);
-    }
-  };
-
-  if (isRecipeView && selectedRecipe) {
-    return <RecipeItem recipeItem={selectedRecipe} />;
-  }
+  // const selectRecipe = (recipeId) => data.find((r) => r._id === recipeId);
 
   if (loading && !error) {
     return <LoadingSpinner />;
@@ -37,17 +24,19 @@ function Recipes() {
     // eslint-disable-next-line array-callback-return
     const recipeItems = data.map((recipe) => (
       <Grid
-        onClick={() => selectRecipe(recipe._id)}
         key={recipe._id}
         item
         xs={6}
         sm={6}
         md={3}
       >
-        <div className="recipe-item__wrapper">
-          <img alt="" src={logo} />
-          <h3>{recipe.name}</h3>
-        </div>
+        <Link to={`/recipes/view/${recipe._id}`} state={recipe}>
+          <div className="recipe-item__wrapper">
+            <img alt="" src={logo} />
+            <h3>{recipe.name}</h3>
+          </div>
+        </Link>
+
       </Grid>
     ));
     return (
--- frontend/src/ViewRecipe.jsx
+++ frontend/src/ViewRecipe.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+// import PropTypes from "prop-types";
 
 function ViewRecipe() {
+  const location = useLocation();
+  console.log(location);
+  // const { fuck } = location.state;
+  // console.log(fuck);
+
   return (
     <h2>View Recipe Page</h2>
   );
 }
+
+// ViewRecipe.propTypes = {
+//   fuck: PropTypes.string.isRequired
+// };
+
 export default ViewRecipe;
