0bb74f094f16c2b565705d6f0120ebcf59ee5006
Max Stevenson <maxstevenson@msn.com>
2022-02-18 21:34:30 -0600

    - Added proptypes for FormInput component.

--- frontend/src/FormInputs.jsx
+++ frontend/src/FormInputs.jsx
@@ -1,14 +1,33 @@
 import React from "react";
-// import PropTypes from "prop-types";
+import PropTypes from "prop-types";
 
-function FormInputs() {
+function FormInputs({ nextStep, formState }) {
   return (
     <div>
       <h2>Its working</h2>
+      <button type="button" onClick={nextStep}>Continue</button>
+      <h2>{formState.servings}</h2>
     </div>
   );
 }
 
-FormInputs.propTypes = {};
+FormInputs.propTypes = {
+  nextStep: PropTypes.func.isRequired,
+  formState: PropTypes.shape({
+    name: PropTypes.string,
+    servings: PropTypes.number,
+    cookingTime: PropTypes.number,
+    isVegetarian: PropTypes.bool,
+    ingredients: PropTypes.arrayOf(
+      PropTypes.shape({
+        quantity: PropTypes.number,
+        measure: PropTypes.string,
+        ingredient: PropTypes.string
+      })
+    ),
+    method: PropTypes.arrayOf(PropTypes.string),
+    tags: PropTypes.arrayOf(PropTypes.string)
+  }).isRequired
+};
 
 export default FormInputs;
