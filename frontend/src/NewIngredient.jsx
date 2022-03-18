0cbef29ee7c5e79cf9b49ddc3ea0a3f8c0d6a122
Max Stevenson <maxstevenson@msn.com>
2022-03-17 21:42:33 -0500

    - WIP ingredient validation.

--- frontend/src/NewIngredient.jsx
+++ frontend/src/NewIngredient.jsx
@@ -1,24 +1,24 @@
 import React, { useState } from "react";
+import validator from "validator";
 import PropTypes from "prop-types";
 import { Button, Container, TextField } from "@mui/material";
 
 function NewIngredient({ handleAddIngredient }) {
-  const [error, setError] = useState(false);
+  const [ingredientValid, setIngredientValid] = useState(false);
 
   const handleAdd = (event) => {
     event.preventDefault();
     const inputs = event.target.parentElement.querySelectorAll("input");
     const ingredient = {
-      quantity: parseInt(inputs[0].value, 10),
+      quantity: inputs[0].value,
       measure: inputs[1].value,
       ingredient: inputs[2].value
     };
-    setError(handleAddIngredient(ingredient));
+    handleAddIngredient(ingredient);
   };
 
   return (
     <Container>
-      {error && <p>{error}</p>}
       <TextField
         required
         type="number"
@@ -30,7 +30,7 @@ function NewIngredient({ handleAddIngredient }) {
       />
       <TextField required variant="outlined" label="Measure" />
       <TextField required variant="outlined" label="Ingredient" />
-      <Button onClick={(event) => handleAdd(event)}>Add Ingredient</Button>
+      <Button variant="contained" disabled={ingredientValid} onClick={(event) => handleAdd(event)}>Add Ingredient</Button>
     </Container>
   );
 }
