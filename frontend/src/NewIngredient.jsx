4f83b03576bb326ac78867a446ae24ab3bc1a030
Max Stevenson <maxstevenson@msn.com>
2022-03-18 22:31:24 -0500

    - WIP ingredient validation.

--- frontend/src/NewIngredient.jsx
+++ frontend/src/NewIngredient.jsx
@@ -1,20 +1,36 @@
 import React, { useState } from "react";
-import validator from "validator";
+// import validator from "validator";
 import PropTypes from "prop-types";
 import { Button, Container, TextField } from "@mui/material";
 
 function NewIngredient({ handleAddIngredient }) {
-  const [ingredientValid, setIngredientValid] = useState(false);
+  const [ingredient, setIngredient] = useState({
+    quantity: { value: 0, isValid: false, errorMsg: "" },
+    measure: { value: "", isValid: false, errorMsg: "" },
+    ingredient: { value: "", isValid: false, errorMsg: "" },
+    isValid: false
+  });
+
+  const handleChange = (inputName, event) => {
+    switch (inputName) {
+      case "quantity":
+        const inputValue = event.target.value
+        setIngredient({});
+        break;
+      case "measure":
+        break;
+      case "ingredient":
+        break;
+
+      default:
+    }
+    // setIngredient((previous) => ({ ...previous }));
+  };
 
   const handleAdd = (event) => {
     event.preventDefault();
-    const inputs = event.target.parentElement.querySelectorAll("input");
-    const ingredient = {
-      quantity: inputs[0].value,
-      measure: inputs[1].value,
-      ingredient: inputs[2].value
-    };
     handleAddIngredient(ingredient);
+    // restore state to 0;
   };
 
   return (
@@ -22,15 +38,17 @@ function NewIngredient({ handleAddIngredient }) {
       <TextField
         required
         type="number"
+        onChange={(event) => { handleChange("quantity", event); }}
         variant="outlined"
         InputLabelProps={{
           shrink: true
         }}
         label="Quantity"
       />
+      {ingredient.quantity.errorMsg && <p>{ingredient.quantity.errorMsg}</p>}
       <TextField required variant="outlined" label="Measure" />
       <TextField required variant="outlined" label="Ingredient" />
-      <Button variant="contained" disabled={ingredientValid} onClick={(event) => handleAdd(event)}>Add Ingredient</Button>
+      <Button variant="contained" disabled={ingredient.isValid} onClick={(event) => handleAdd(event)}>Add Ingredient</Button>
     </Container>
   );
 }
