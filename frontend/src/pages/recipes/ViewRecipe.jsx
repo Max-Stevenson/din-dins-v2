0ab3938aa569bad5976e0db8a9381159888c0337
Max Stevenson <maxstevenson@msn.com>
2022-06-01 18:02:44 -0500

    - ViewRecipe wip.

--- frontend/src/pages/recipes/ViewRecipe.jsx
+++ frontend/src/pages/recipes/ViewRecipe.jsx
@@ -1,18 +1,17 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-import { useLocation } from "react-router";
-// import { useParams } from "react-router";
+import { useLocation, useParams } from "react-router";
 // import LoadingSpinner from "../../shared/components/LoadingSpinner";
-// import useFetch from "../../shared/hooks/useFetchHook";
+import useFetch from "../../shared/hooks/useFetchHook";
 
 function ViewRecipe() {
-  const { state } = useLocation();
-  const { recipe } = state;
-  // const { recipe } = state;
-  // const { recipeId } = useParams();
-  // const { data, error, loading } = useFetch(
-  //   `http://localhost:3000/api/v1/recipes/${recipeId}`
-  // );
+  const { recipe } = useLocation().state;
+  if (!recipe) {
+    const { recipeId } = useParams();
+    const { data, error, loading } = useFetch(
+      `http://localhost:3000/api/v1/recipes/${recipeId}`
+    );
+  }
 
   // if (loading && !error) {
   //   return <LoadingSpinner />;
