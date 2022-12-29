6411952e4b6d8b7711a8abb111146ce712eaafe7
Max Stevenson <maxstevenson@msn.com>
2022-12-28 20:24:21 -0600

    - auth now verifies user status.

--- backend/src/middleware/check-auth.js
+++ backend/src/middleware/check-auth.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 const HttpError = require("../models/httpError");
+const User = require("../models/user");
 
 dotenv.config();
 const { JWT_TOKEN } = process.env;
@@ -8,13 +9,20 @@ const { JWT_TOKEN } = process.env;
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    console.log(token);
     if (!token) {
       throw new Error("Authentication failed");
     }
     const decodedToken = jwt.verify(token, JWT_TOKEN);
-    console.log(decodedToken);
-    req.userData = { userId: decodedToken.userId };
+    if (decodedToken) {
+      const decodedTokenUserId = decodedToken.userId;
+      if (decodedTokenUserId) {
+        User.findOne({ decodedTokenUserId }).then((user) => {
+          if (!user) {
+            throw new Error();
+          }
+        });
+      }
+    }
     return next();
   } catch (err) {
     const error = new HttpError("Authentication failed", 401);
