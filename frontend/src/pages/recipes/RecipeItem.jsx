d1671dc432574412e2a8ef60ac71b8d3fc800c2b
Max Stevenson <maxstevenson@msn.com>
2023-03-16 14:07:04 -0500

    - RecipeItem updated to have keys on li elements.

--- frontend/src/pages/recipes/RecipeItem.jsx
+++ frontend/src/pages/recipes/RecipeItem.jsx
@@ -1,18 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import PropTypes from "prop-types";
 import { Tab, Tabs } from "@mui/material";
 import TabPanel from "../../shared/components/TabPanel";
+import { hashCode } from "../../shared/utils/hashCode";
 
 function RecipeItem({ recipeItem }) {
   const [tabValue, setTabValue] = useState(0);
-  const handleChange = (_event, value) => {
+  const handleChange = useCallback((_event, value) => {
     setTabValue(value);
-  };
+  }, []);
 
   return (
     <div className="recipe-item__wrapper">
       <div className="recipe-item__img-wrapper">
-        <img alt="" src="/images/recipe.jpg" />
+        <img alt={recipeItem.name} src="/images/recipe.jpg" />
       </div>
       <h3>{recipeItem.name}</h3>
       <Tabs value={tabValue} onChange={handleChange}>
@@ -23,7 +24,7 @@ function RecipeItem({ recipeItem }) {
         <h2>Ingredients</h2>
         <ul>
           {recipeItem.ingredients.map((i) => (
-            <li>
+            <li key={hashCode(`${i.quantity}${i.measure}${i.ingredient}`)}>
               {i.quantity}
               {" "}
               {i.measure}
@@ -37,7 +38,7 @@ function RecipeItem({ recipeItem }) {
         <h2>Method</h2>
         <ol>
           {recipeItem.method.map((method) => (
-            <li>
+            <li key={hashCode(method.method)}>
               {method.method}
             </li>
           ))}
